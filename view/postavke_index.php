e7bd26d6f8b16d51a783ab3a0fc1f8fbabcfdef1
Zaja Jelena <jzaja@pr4-03.studenti.math.hr>
2025-05-20 11:31:14 +0200

    promjena username/pass

--- view/postavke_index.php
+++ view/postavke_index.php
@@ -1,6 +1,12 @@
 <?php require_once __SITE_PATH . '/view/_header.php'; ?>
 
-Dobar dan!
-ovo su postavke!!!!!
+<form action="" method="post">
+    Promijeni svoj username: <input type="text" name="promjena_username">
+    <button type="submit" name="gumb" value="btn_username">Promijeni!</button>
+    <br>
+    Promijeni svoju šifru: <input type="password" name="promjena_pass">
+    <button type="submit" name="gumb" value="btn_pass">Promijeni!</button>
+    <br>
+</form>
 
 <?php require_once __SITE_PATH . '/view/_footer.php'; ?>
