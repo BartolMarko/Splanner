3707c4e79cb1c5824aa9d0c334e5f5e93aee2656
BartolMarko <bartolmarkovinovic@gmail.com>
2025-07-09 22:47:50 +0200

    fixan merge postavki

--- view/login_index.php
+++ view/login_index.php
@@ -25,15 +25,6 @@
     </p>
 
     <?php if (isset($error)) echo '<p class="centered-text" style="color:red; margin-top: 20px;">' . $error . '</p>'; ?>
-</div>
-
-    <p class="centered-text">
-        <br>
-        Ako i dalje niste dio našeg tima, sada je pravo vrijeme da to promijenimo! <br><br>
-        <a href="<?php echo __SITE_URL; ?>/index.php?rt=login/registracija" class="button-link">Registracija</a>
-    </p>
-
-    <?php if (isset($error)) echo '<p class="centered-text" style="color:red; margin-top: 20px;">' . $error . '</p>'; ?>
 
 </div>
 
