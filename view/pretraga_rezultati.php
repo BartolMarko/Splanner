b3ad11ac03afcae70c6b7bb11a2e6bd17c095ddf
jelenazxy1@gmail.com <jelenazxy1@gmail.com>
2025-07-11 21:07:30 +0200

    promjena

--- view/pretraga_rezultati.php
+++ view/pretraga_rezultati.php
@@ -4,7 +4,7 @@
 
 <?php if (!empty($rezultati)): ?>
 	<?php foreach ($rezultati as $rez): ?>
-		<div class="rezpret kosarica" onclick="window.location.href='index.php?rt=aktivnosti/grupa&id=<?php echo $rez['id_grupe']; ?>'">
+		<div class="rezpret kosarica"  style="cursor:pointer;" onclick="window.location.href='index.php?rt=aktivnosti/grupa&id=<?php echo $rez['id_grupe']; ?>&from=pretraga'">
 			<strong><?= htmlspecialchars($rez['aktivnost_ime']) ?></strong><br>
 			Grupa: <?= htmlspecialchars($rez['grupa_ime'])?><br>
 			Cijena: <?= htmlspecialchars($rez['cijena']) ?> EUR<br>
